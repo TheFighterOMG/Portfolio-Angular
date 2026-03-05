b784926e54b8ece57adedb7fa254b5e9cbef7911
TheFighterOMG <dnlgmzrtz@gmail.com>
2026-03-05 21:16:34 +0100

    build: actualización a Angular 19 completada

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from '../../services/portfolio.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    standalone: false
 })
 export class HomeComponent implements OnInit {
   personalData: any;
--- src/app/views/skills/skills.component.ts
+++ src/app/views/skills/skills.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from '../../services/portfolio.service';
 
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+    selector: 'app-skills',
+    templateUrl: './skills.component.html',
+    styleUrls: ['./skills.component.css'],
+    standalone: false
 })
 export class SkillsComponent implements OnInit {
   skills: any[] = [];
